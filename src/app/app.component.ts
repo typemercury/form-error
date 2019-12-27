ee58337ac1bca6afc61d477a0d1ac1d5d8fbe406
洪佳興 Josh <josh@miniasp.com>
2019-12-27 11:42:49 +0800

    add simple form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'form-error';
+export class AppComponent implements OnInit {
+  form: FormGroup;
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      address: this.fb.group({
+        city: ['', Validators.required],
+        country: ['', Validators.required]
+      }),
+      terms: ['', Validators.requiredTrue]
+    });
+  }
 }
